5cdeebb8bb6bebc0523b3f20868566677b410691
Aastha Kasera <45789780+aasthakasera@users.noreply.github.com>
2022-03-14 13:01:08 +0530

    add flies

---
+++ index.js
@@ -0,0 +1,22 @@
+let openFAQ = document.getElementsByClassName("FAQ__title");
+
+for (let i = 0; i < openFAQ.length; i++) {
+  openFAQ[i].addEventListener("click", function () {
+    //open content
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    //check height
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
